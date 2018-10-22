631716f64d118f952bb667130f30c621e375eab7
murali <murali@test.com>
2018-10-22 22:37:00 +0530

    initial check in

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to DevOps");
+            Console.ReadKey();
         }
     }
 }
